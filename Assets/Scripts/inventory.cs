b0cea1d6ba2068cecb8e420a5174ef62553f1317
MV Coder <mvcoder@MVCC-MACMINI-53.local>
2017-01-10 16:18:17 -0800

    added the inventory script

---
+++ Assets/Scripts/inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour {
+
+	class invObj {
+		string name;
+		int amount;
+
+		public invObj (string n, int q) {
+			name = n;
+			amount = q;
+		}
+	}
+
+	List<invObj> inv;
+
+	public void AddToList (string n, int q) {
+		inv.Add(new invObj(n,q));
+	}
+
+	void Start () {
+		inv = new List<invObj> ();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.A)) {
+		}
+	}
+}
