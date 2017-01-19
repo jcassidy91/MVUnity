89e85b747cbf601e1af605c460c1e9b9e3aa7260
MV Coder <mvcoder@MVCC-MACMINI-53.local>
2017-01-18 20:09:33 -0800

    added models/spin

---
+++ Assets/Scripts/spinningControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinningControl : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject pointLight;
+	public Quaternion rotatePosition;
+	// Use this for initialization
+	void Start () {
+		rotatePosition = player.transform.rotation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver() {
+		player.transform.Rotate (Vector3.up * (180 * Time.deltaTime));
+		pointLight.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.962416f, -4.79f);
+		pointLight.GetComponent<Light> ().intensity = 8;
+	}
+
+	void OnMouseExit() {
+		pointLight.GetComponent<Light> ().intensity = 0;
+		player.transform.rotation = rotatePosition;
+	}
+}
