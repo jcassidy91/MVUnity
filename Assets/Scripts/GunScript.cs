98c43819ce246734cd3949bb4842dc3a9461cf7f
MV Coder <mvcoder@MVCC-MACMINI-8.local>
2017-03-01 20:04:03 -0800

    trying to add gun

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+	GameObject player;
+
+	void Start () {
+		player = gameObject;
+	}
+
+	void Update () {
+		Shoot ();
+	}
+
+	void Shoot() {
+
+		if (Input.GetKey (KeyCode.Mouse0)) {
+
+
+			var pos = player.transform.position;
+			var rot = new Vector3(player.transform.eulerAngles.x, GameObject.Find("Main Camera").transform.eulerAngles.y, player.transform.eulerAngles.z);
+			Debug.DrawRay (pos, rot, Color.blue, 2, true);
+		}
+
+	}
+}
